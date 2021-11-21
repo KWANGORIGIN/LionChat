d1e3ef7d25e080db549ec608c01bcea270d41af3
Kevin <wangatang53@gmail.com>
2021-11-21 15:14:14 -0500

    Initial commit of Spring Boot project

---
+++ Lionchat/src/main/java/com/psu/Lionchat/LionchatApplication.java
@@ -0,0 +1,13 @@
+package com.psu.Lionchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LionchatApplication.class, args);
+	}
+
+}
---
+++ Lionchat/src/test/java/com/psu/Lionchat/LionchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psu.Lionchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
