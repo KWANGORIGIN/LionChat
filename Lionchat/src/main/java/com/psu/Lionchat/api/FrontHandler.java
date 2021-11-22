92392ebfdc3b97545e98d6f2cfb783acc5a66382
William <willhemminger30@gmail.com>
2021-11-21 19:59:43 -0500

    removed redundant String.format

--- Lionchat/src/main/java/com/psu/Lionchat/api/FrontHandler.java
+++ Lionchat/src/main/java/com/psu/Lionchat/api/FrontHandler.java
@@ -1,10 +1,8 @@
 package com.psu.Lionchat.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class FrontHandler {
@@ -12,7 +10,7 @@ public class FrontHandler {
     @RequestMapping("/mypage/{endpoint}")
     public String getFromEndpoint(@PathVariable("endpoint") String endpoint)
     {
-        System.out.println(String.format("Forwarded to handler forwarded%s", endpoint));
+        System.out.printf("Forwarded to handler forwarded%s%n", endpoint);
 
         return String.format("forward:/forwarded%s", endpoint);
     }
