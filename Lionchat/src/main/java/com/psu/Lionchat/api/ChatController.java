3cd34f2288b081e5dadf4c9c6da79d87c77f5238
William <willhemminger30@gmail.com>
2021-11-26 12:53:08 -0500

    Fixed service issue poorly.  Should consider a proxy instead.

--- Lionchat/src/main/java/com/psu/Lionchat/api/ChatController.java
+++ Lionchat/src/main/java/com/psu/Lionchat/api/ChatController.java
@@ -1,20 +1,27 @@
 package com.psu.Lionchat.api;
 
 import com.psu.Lionchat.service.AbstractService;
+import com.psu.Lionchat.service.ChatServiceImpl;
+import com.psu.Lionchat.service.FeedbackService;
+import com.psu.Lionchat.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatController {
-    /*
-    private final AbstractService abstractService;
+
+    private final ChatServiceImpl chatService;
+    private final RatingService ratingService;
+    private final FeedbackService feedbackService;
 
     @Autowired
-    public ChatController(AbstractService abstractService)
+    public ChatController(ChatServiceImpl chatService, RatingService ratingService, FeedbackService feedbackService)
     {
-        this.abstractService = abstractService;
+        this.chatService = chatService;
+        this.ratingService = ratingService;
+        this.feedbackService = feedbackService;
     }
-     */
+
 
     // add mappings for requests
 }
