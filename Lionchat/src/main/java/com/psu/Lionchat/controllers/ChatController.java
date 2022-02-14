87624a40a5d8eed62586ec4d4a353de428552817
jxk5861 <jxk5861@psu.edu>
2022-02-13 19:12:28 -0500

    Unused import.

--- Lionchat/src/main/java/com/psu/Lionchat/controllers/ChatController.java
+++ Lionchat/src/main/java/com/psu/Lionchat/controllers/ChatController.java
@@ -3,7 +3,6 @@ package com.psu.Lionchat.controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
