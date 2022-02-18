797755feed765d7ed9393141cc22a632fcb8d464
jxk5861 <jxk5861@psu.edu>
2022-02-18 01:15:19 -0500

    Queries /chat/askquestion on question asked.

--- Lionchat/react-frontend/src/ChatBox/ChatBox.js
+++ Lionchat/react-frontend/src/ChatBox/ChatBox.js
@@ -8,60 +8,88 @@ import { v4 as uuidv4 } from 'uuid'
 const LOCAL_STORAGE_KEY = 'LionChat.Messages'
 
 const ChatBox = () => {
-    const [messages, setMessages] = useState([]/*JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))*/)
-    const [minimized, setMinimized] = useState(true)
-    const chatInput = useRef()
+	const [messages, setMessages] = useState([]) // Don't parse local storage in here, it could be null.
+	const [minimized, setMinimized] = useState(true)
+	const chatInput = useRef()
 
-    useEffect(() => {
-        const storedMessages = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-        if (storedMessages) {
-            //setMessages(storedMessages)
-        }
-    }, [])
+	useEffect(() => {
+		const storedMessages = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+		if (storedMessages) {
+			setMessages(storedMessages)
+		}
+	}, [])
 
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(messages))
-    }, [messages])
+	useEffect(() => {
+		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(messages))
+	}, [messages])
 
-    function handleSendMessage() {
-        const message = chatInput.current.value
-        if (message === "") {
-            return
-        }
+	async function handleSendMessage() {
+		const message = chatInput.current.value
+		if (message === 'clear') {
+			chatInput.current.value = null
+			setMessages([])
+			return
+		}
 
-        setMessages(previousMessages => {
-            return [...previousMessages, { id: uuidv4(), text: message, userSent: messages.length % 2 === 0 }]
-        })
-        chatInput.current.value = null
-    }
+		if (message === "") {
+			return
+		}
+
+		chatInput.current.value = null
+		setMessages(previousMessages => {
+			return [...previousMessages, { id: uuidv4(), text: message, userSent: true }]
+		})
+
+
+		const data = { question: message }
+
+		const response = await fetch(`/chat/askquestion`,
+			// TODO: Create an object for this...
+			{
+				method: 'POST',
+				mode: 'cors',
+				cache: 'no-cache',
+				credentials: 'same-origin',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				// redirect: 'follow',
+				body: JSON.stringify(data),
+			})
+
+		const chatResponse = await response.text()
+		setMessages(previousMessages => {
+			return [...previousMessages, { id: uuidv4(), text: chatResponse, userSent: false }]
+		})
+	}
 
 
 
-    if (minimized) {
-        return <ChatBubble maximize={() => setMinimized(false)} />
-    }
+	if (minimized) {
+		return <ChatBubble maximize={() => setMinimized(false)} />
+	}
 
-    const chatBoxStyle = {
-        width: 300,
-        position: 'fixed',
-        bottom: 10,
-        right: 10,
-        background: '#ffffff',
-        height: 400,
-        boxShadow: 'rgb(0 0 0 / 20%) 0px 0px 12px',
-        display: 'flex',
-        flexDirection: 'column',
-        userSelect: 'none',
-        zIndex: 100000
-    }
+	const chatBoxStyle = {
+		width: 300,
+		position: 'fixed',
+		bottom: 10,
+		right: 10,
+		background: '#ffffff',
+		height: 400,
+		boxShadow: 'rgb(0 0 0 / 20%) 0px 0px 12px',
+		display: 'flex',
+		flexDirection: 'column',
+		userSelect: 'none',
+		zIndex: 100000
+	}
 
-    return (
-        <div style={chatBoxStyle}>
-            <ChatHeader minimize={() => setMinimized(true)} />
-            <ChatMessages messages={messages} />
-            <ChatInput chatInput={chatInput} handleSendMessage={handleSendMessage} />
-        </div>
-    )
+	return (
+		<div style={chatBoxStyle}>
+			<ChatHeader minimize={() => setMinimized(true)} />
+			<ChatMessages messages={messages} />
+			<ChatInput chatInput={chatInput} handleSendMessage={handleSendMessage} />
+		</div>
+	)
 }
 
 export default ChatBox
