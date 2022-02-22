14762df1600d8fb40c8896a02e5a265fb0af9907
jxk5861 <jxk5861@psu.edu>
2022-02-22 00:38:49 -0500

    If helpful is selected, opacity increases. This does not save on refresh so that needs fixed.

--- Lionchat/react-frontend/src/ChatBox/ChatMessageFooter.js
+++ Lionchat/react-frontend/src/ChatBox/ChatMessageFooter.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ChatMessageFooter.module.css"
 
 //😁🙂😃😢😟😥😖👍👎
 const ChatMessageFooter = ({ sendFeedback }) => {
+    const [helpfulSelected, setHelpfulSelected] = useState(false);
+    const [unhelpfulSelected, setUnhelpfulSelected] = useState(false);
+
+    const setHelpful = (helpful) => {
+        // sendFeedback(helpful)
+
+        setHelpfulSelected(helpful)
+        setUnhelpfulSelected(!helpful)
+    }
+
     return (
         <div className={styles.footerBubbleContainer}>
             <div className={styles.feedbackBubble}>
-                <label className={styles.feedbackEmoji} onClick={() => sendFeedback(true)}>🤠</label>
-                <label className={styles.feedbackEmoji} onClick={() => sendFeedback(false)}>🤮</label>
+                <label className={helpfulSelected ? styles.feedbackEmojiSelected : styles.feedbackEmoji} onClick={() => setHelpful(true)}>🤠</label>
+                <label className={unhelpfulSelected ? styles.feedbackEmojiSelected : styles.feedbackEmoji} onClick={() => setHelpful(false)}>🤮</label>
             </div>
             <div className={styles.bubbleWhiteBackground}>
                 <div className={styles.footerBubbleRounded} />
