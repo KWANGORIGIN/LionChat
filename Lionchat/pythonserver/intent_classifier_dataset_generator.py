e0c643e81faac0110ed37fa883f4f7b735e6ff56
Kevin <wangatang53@gmail.com>
2022-03-10 15:03:18 -0500

    Added intent classifier dataset generator file

---
+++ Lionchat/pythonserver/intent_classifier_dataset_generator.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 09:13:26 2022
+
+@author: Kevin Wang
+"""
+from nltk.corpus import stopwords
+
+class intent_classifier_dataset_generator:
+    
+    alpha = .80
+    
+    def remove_stopwords(dataset):
+        pass
+    
+    def synonym_replacement(sentence):
+        pass
+    
+    def random_insertion(sentence):
+        pass
+    
+    def random_swap(sentence):
+        pass
+    
+    def random_deletion(sentence):
+        pass
+    
+if __name__ == "__main__":
+    
+    dataset_generator = intent_classifier_dataset_generator()
