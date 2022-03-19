98d536fa795b946d2e4b1b583cb41be5056f1f57
Kevin <wangatang53@gmail.com>
2022-03-19 11:33:26 -0400

    Implemented search strategy

--- Lionchat/src/main/java/com/psu/Lionchat/service/ai/SearchStrategy.java
+++ Lionchat/src/main/java/com/psu/Lionchat/service/ai/SearchStrategy.java
@@ -2,7 +2,8 @@ package com.psu.Lionchat.service.ai;
 
 public class SearchStrategy extends IntentStrategyAbs{
     @Override
+    //Returns search string from Google
     public String doStrategy(String question) {
-        return null;
+        return "http://www.google.com/search?q=" + question;
     }
 }
