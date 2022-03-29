0444b07699a709cc0b4ebf1be2c9912343a73b0d
William <willhemminger30@gmail.com>
2022-03-29 18:26:40 -0400

    new events branch

--- Lionchat/pythonserver/LionchatBrain.py
+++ Lionchat/pythonserver/LionchatBrain.py
@@ -14,9 +14,21 @@ import ner
 import tox
 import csv
 import spacy
+import yaml
+from flask_mysqldb import MySQL
+from datetime import datetime
+from datetime import tzinfo, timedelta, datetime
 
 app = Flask(__name__)
 CORS(app)
+
+#configure database info
+db = yaml.safe_load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mydb = MySQL(app)
 
 #Sentence Transformer embedder
 embedder = SentenceTransformer('all-MiniLM-L6-v2')
@@ -77,6 +89,7 @@ def getSemanticSearchResults():
     #Return json list of search results
     return jsonify(results)
 
+
 nlp = spacy.load("./output_updated/model-best")
 @app.before_first_request
 def trainIntentClassifier():
@@ -134,21 +147,129 @@ def classifyIntent():
     
     #Return intent as JSON
     return jsonify(intent=classifiedIntent)
+
+@app.route('/answer_events', methods=["POST"])
+def get_events():
+    #default message
+    message = "Here's what I found for this week:"
     
-@app.route('/events_entities', methods=["POST"])
-def get_entities():
-    
+    #get request from Spring Boot application containing query
     if isinstance(request.json, str):
         text = jsonify(request.json)["text"]
     else:
         text = request.json["text"]
     
-    entities = []
+    entities = ner.getEnts(text) # get entities from user query
     
-    for ent in ner.getEnts(text):
-        entities.append((ent.text, ent.label_))
+    #if entities are present
+    if entities:
+        #initialize some booleans
+        more_entities = False
+        set_date = False
+        set_name = False
+        sql = "SELECT * FROM events WHERE " # begin sql query on events table
         
-    return jsonify(entities = entities)
+        #find current day for use in query
+        current_day = datetime.today().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+        date1 = current_day
+        date2 = date1
+        
+        #by default, get events happening this week, so time span is set for current day till end of week
+        while date2.weekday() != 6:
+            date2 += timedelta(days=1)
+        date2 += timedelta(days=1)
+
+        
+        for ent in entities:
+            label = ent.label_
+            text = ent.text.replace("?", "") #replace question mark if was accidentally returned
+            
+            if label == 'EVTNAME': #if event name was input, get all future dates instead of one week
+                set_name = True
+            
+            if more_entities and label != 'DATE': #if date was entered, set time span accordingly
+                sql = sql + " AND " 
+            
+            if label == 'DATE':
+                set_date = True
+
+                if text == 'today':
+                    date2 = date1 + timedelta(days=1)
+                    message = "Here's what I found for this today:"
+
+                elif text == 'tomorrow':
+                    date1 = current_day + timedelta(days=1)
+                    date2 = date1 + timedelta(days=1)
+                    message = "Here's what I found for tomorrow:"
+                
+                elif text == 'next week':
+                    date1 += timedelta(weeks = 1)
+                    date2 = date1
+                    
+                    while date1.weekday() != 0:
+                        date1 -= timedelta(days = 1)
+                    while date2.weekday() != 6:
+                        date2 += timedelta(days = 1)
+                    date2 += timedelta(days=1)   
+                    message = "Here's what I found for next week:"
+                    
+                elif text == 'this month':
+                    date2 = date1
+                    month = date1.month
+                    while date2.month == month:
+                        date2 += timedelta(days = 1)
+                    message = "Here's what I found for this month:"
+                elif text != 'this week':
+                    message = "I'm sorry, that time-frame was not recognized.  Here are today's events:"
+                    date2 = date1 + timedelta(days=1)
+
+            else:
+                sql = sql +  "LOWER({}) like '%{}%'".format(label, text)
+                more_entities = True
+            
+        for ent in entities:
+            print(ent.label_, ent.text)
+            
+        if more_entities:
+            sql = sql + " AND " #add more entities to search in the database
+            
+        #if event name was set
+        if set_name:
+            message = "Here's what I found for this event:"
+     
+        if not set_date and set_name: #if name entered and date not entered, query all future dates for event
+            sql += "DATE >= {}".format(int(date1.timestamp()))
+        else: #otherwise query between whatever dates were set
+            sql += "DATE BETWEEN {} AND {}".format(int(date1.timestamp()), int(date2.timestamp()) - 1)
+        print(sql)
+        print(date1)
+        print(datetime.fromtimestamp(date2.timestamp() - 1))
+        cursor = mydb.connection.cursor()
+        cursor.execute(sql)
+        
+        evt_list = list(cursor)
+        
+        for i in range(len(evt_list)):
+            evt_list[i] = list(evt_list[i])
+            evt_list[i][3] = str(datetime.fromtimestamp(int(evt_list[i][3])).strftime("%m/%d/%Y, %H:%M:%S"))
+        
+        if evt_list: #if events found
+            payload = jsonify(message = message, events = evt_list)
+        else: #if nothing found
+            payload = jsonify(message = "It looks like I don't have any information on that.", events = [])
+        
+        cursor.close()
+    else: #if no entities were present
+        payload = jsonify(message = 'I am sorry, I need more data.  Can you be more specific?', events = [])
+        
+    
+     
+    #EVTNAME
+    #LOC
+    #DATE
+    #ORG
+    
+    return payload
 
 @app.route('/toxic_classification', methods=["POST"])
 def is_toxic():
