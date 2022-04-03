b3ec6dc939e1ec1da87877ac4ee80e6b2b99d8da
jxk5861 <jxk5861@psu.edu>
2022-04-03 02:20:57 -0400

    Improved send button, added an invisible div with 1vw to give a space for where the scrollbar would be.

--- Lionchat/react-frontend/src/ChatBox/ChatInput.js
+++ Lionchat/react-frontend/src/ChatBox/ChatInput.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import styles from './ChatInput.module.css'
+import React from "react";
+import styles from "./ChatInput.module.css";
 
 const ChatInput = ({ chatInput, handleSendMessage }) => {
-    function chatInputKeyPress(e) {
-        if (e.code === "Enter") {
-            handleSendMessage()
-        }
-
+  function chatInputKeyPress(e) {
+    if (e.code === "Enter") {
+      handleSendMessage();
     }
-    return (
-        <div className={styles.container}>
-            <input className={styles.messageBox} ref={chatInput} type="text" onKeyPress={chatInputKeyPress} />
-            <button className={styles.send} onClick={handleSendMessage}>Send</button>
-        </div>
-    )
-}
+  }
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.messageBox}
+        ref={chatInput}
+        type="text"
+        onKeyPress={chatInputKeyPress}
+      />
+      {/* <button className={styles.send} onClick={handleSendMessage}>Send</button> */}
+      {/* <img className={styles.send} src={Send}></img> */}
+      <div className={styles.send} onClick={handleSendMessage}></div>
+      <div className={styles.scrollbarSpacer}></div>
+    </div>
+  );
+};
 
-export default ChatInput
+export default ChatInput;
