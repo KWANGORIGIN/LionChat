08e14c629a31f4f9b9c76252453d330a35b7025d
jxk5861 <jxk5861@psu.edu>
2022-04-03 21:11:25 -0400

    small docker changes

--- Lionchat/pythonserver/db/ml_database.sql
+++ Lionchat/pythonserver/db/ml_database.sql
@@ -18,9 +18,6 @@
 --
 -- Table structure for table `events`
 --
-CREATE DATABASE ml_database;
-use ml_database;
-
 DROP TABLE IF EXISTS `events`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
