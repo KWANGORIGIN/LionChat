78d4e3189677ff82a2472f90acd38ae0120f29f2
jxk5861 <jxk5861@psu.edu>
2022-04-11 00:31:26 -0400

    URL Encoding bug fix

--- Lionchat/src/main/java/com/psu/Lionchat/service/ai/SearchStrategy.java
+++ Lionchat/src/main/java/com/psu/Lionchat/service/ai/SearchStrategy.java
@@ -1,9 +1,20 @@
 package com.psu.Lionchat.service.ai;
 
-public class SearchStrategy extends IntentStrategyAbs{
-    @Override
-    //Returns search string from Google
-    public String doStrategy(String question) {
-        return "http://www.google.com/search?q=" + question;
-    }
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class SearchStrategy extends IntentStrategyAbs {
+	@Override
+	// Returns search string from Google
+	public String doStrategy(String question) {
+		try {
+			question = URLEncoder.encode(question, StandardCharsets.UTF_8.toString());
+			String search = String.format("http://www.google.com/search?q=%s", question);
+			return String.format("<a href=%s target=\"_blank\">%s<" + "/a>", search, search);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return "Failed to create google search. (invalid character present?)";
+		}
+	}
 }
