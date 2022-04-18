53245dae596c3c623f343d44ae0c6fa3e84c2f05
jxk5861 <jxk5861@psu.edu>
2022-04-18 17:13:17 -0400

    disable csrf but really should find a way to enable it

--- Lionchat/src/main/java/com/psu/Lionchat/SecurityConfiguration.java
+++ Lionchat/src/main/java/com/psu/Lionchat/SecurityConfiguration.java
@@ -1,18 +1,16 @@
 package com.psu.Lionchat;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -29,13 +27,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http
 			.authorizeRequests()
 			.antMatchers("/administrative/").hasRole("ADMIN")
-			.and().formLogin();
+			.and().formLogin()
+			// TODO: Enable this and use 
+			// .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			// Then use cookies.get('XSRF-TOKEN') in react.
+			// This is annoying because I think it will break npm proxy
+			.and().csrf().disable();
 	}
 	
-//	@Override
-//	public void configure(WebSecurity web) throws Exception {
-//		web.ignoring().antMatchers("/h2/**");
-//	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2/**");
+	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
